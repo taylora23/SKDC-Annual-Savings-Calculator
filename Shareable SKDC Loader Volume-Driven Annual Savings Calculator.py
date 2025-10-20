39d691ad411fb700a8c97ab433d7495997610423
taylora23 <aisha.taylor@ambitionspersonnel.com>
2025-10-20 10:38:41 +0100

    Add files via upload

---
+++ Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py
@@ -0,0 +1,13 @@
+# === Streamlit Calculator ===
+
+import streamlit as st
+
+st.title("SKDC Loader Volume-Driven Annual Savings Calculator")
+
+fixed_numer = 9436.24
+
+user_input = st.number_input("Enter a number")
+
+result = user_input * fixed_number
+
+st.write(f"Result: {user_input} x {fixed_number} = {result}")
