2fcbb6f5404dd9a8a718e29ac2c2a046fa1c7c08
taylora23 <aisha.taylor@ambitionspersonnel.com>
2025-10-20 11:44:51 +0100

    Update Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py

--- Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py
+++ Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py
@@ -2,7 +2,10 @@
 
 import streamlit as st
 
+st.set_page_config(page_title="SKDC Loader Volume-Driven Annual Savings Calculator", layout="wide")
+
 st.title("SKDC Loader Volume-Driven Annual Savings Calculator")
+st.image("Ambitions Public Sector Logo.png", width=120)
 
 fixed_number = 9436.24
 
@@ -14,5 +17,6 @@ result = user_input * fixed_number
 formatted_result = f"£{result:,.2f}"
 
 st.write(f"Result: {user_input} x £{fixed_number:,.2f} = {formatted_result}")
+
 
 
