8d4de68b0829592feb5a7ac80840a6cb772ad73b
taylora23 <aisha.taylor@ambitionspersonnel.com>
2025-10-20 11:53:56 +0100

    Update Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py

--- Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py
+++ Shareable SKDC Loader Volume-Driven Annual Savings Calculator.py
@@ -10,14 +10,19 @@ st.title("SKDC Loader Volume-Driven Annual Savings Calculator")
 fixed_number = 9436.24
 
 user_input = st.number_input(
-  "Enter a number", min_value=1, step=1, format="%d")
+  "Enter the quantity of workers", min_value=1, step=1, format="%d")
 
 result = user_input * fixed_number
 
 formatted_result = f"£{result:,.2f}"
 
-st.write(f"Result: {user_input} x £{fixed_number:,.2f} = {formatted_result}")
-
-
+st.markdown(
+    f"""
+    <p style='font-size:30px; font-weight:bold; text-align:center;'>
+        Annual Savings Achieved: {user_input} x £{fixed_number:,.2f} = {formatted_result}
+    </p>
+    """,
+    unsafe_allow_html=True
+)
 
 
